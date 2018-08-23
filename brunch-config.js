bcd63532ce06c6ebc584a5d6f43b8a89390ee5ab
Clive Ayonye <cliveua@gmail.com>
2018-08-24 00:35:42 +0200

    Use Brunch as a build tool

---
+++ brunch-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	files: {
+		javascripts: {
+			joinTo: { 'app.js': /^app/ }
+		},
+		stylesheets: { joinTo: 'css/app.css' }
+	},
+	plugins: {
+		babel: { presets: ['env'] },
+		autoReload: { enabled: true },
+		uglify: {
+			mangle: true,
+			compress: {
+				global_defs: {
+					DEBUG: false
+				}
+			}
+		},
+		cleancss: {
+			keepSpecialComments: 0,
+			removeEmpty: true
+		}
+	},
+	overrides: {
+		production: {
+			optimize: true,
+			sourceMaps: false,
+			plugins: { autoReload: { enabled: false } }
+		}
+	},
+	paths: {
+		public: 'dist'
+	},
+	conventions: {
+		assets: [/^(?!app\/(css|js)).*/] // Files not in `css` or `js` dir (e.g img, html).
+	}
+}
